32249e75ee5e5cdbdcf801e02bd056eeb6989d55
lepeha <nostresko@gmail.com>
2024-06-09 22:31:36 +0400

    registr dont work

--- main/views.py
+++ main/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+
 
 def index(request):
     return render(request, 'home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('main')  # укажите вашу страницу после успешной регистрации
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/login.html', {'form': form})
---
+++ market/celery.py
@@ -0,0 +1,9 @@
+import os
+from celery import Celery
+ 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'market.settings')
+ 
+app = Celery('mcdonalds')
+app.config_from_object('django.conf:settings', namespace = 'CELERY')
+
+app.autodiscover_tasks()
